dd9dbbe9338dacaaa5993e845bf726c4ac2a3cb2
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-11 21:25:21 +0500

    Task3

---
+++ Tyuiu.MolokanovNK.Sprint2.Task3.V19.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MolokanovNK.Sprint2.Task3.V19.Lib
+{
+    public class DataService : ISprint2Task3V19
+    {
+        public double Calculate(double x)
+        {
+                double y = 0;
+                if (x > 1)
+                {
+                y = Math.Pow(x, 2) + Math.Pow((x + 1) / (x - 1),x);
+                }
+                else
+                {
+                    if (x == 0)
+                    {
+                        y = (Math.Pow(x, 2) + Math.Cos(Math.Pow(x, 2)) + 13) / (Math.Pow(x, 2) + Math.Sin(Math.Pow(x, 2)) + 9);
+                    }
+                    else
+                    {
+                    if (-23 < x) && (x < 2)
+                        {
+                            y = 1 + (1 / Math.Pow(x, 2))), x);
+                        }
+                        else
+                        {
+                            if (x < -23)
+                            {
+                                y = x + 10 * x - (1 / x);
+                            }
+                        }
+                    }
+                }
+                return Math.Round(y, 3);
+        }
+        
+    }
+}
+    
+
---
+++ Tyuiu.MolokanovNK.Sprint2.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MolokanovNK.Sprint2.Task3.V19.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint2.Task3.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double res = ds.Calculate(x);
+            double wait = 17;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double wait = 13;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -5;
+            double res = ds.Calculate(x);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -15;
+            double wait = -164.933;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
