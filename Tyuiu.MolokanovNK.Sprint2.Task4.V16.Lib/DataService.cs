353b244e8b079f6b9a745234047d3a72dd8bba60
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-12 22:57:53 +0500

    Task4

--- Tyuiu.MolokanovNK.Sprint2.Task4.V16.Lib/DataService.cs
+++ Tyuiu.MolokanovNK.Sprint2.Task4.V16.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MolokanovNK.Sprint2.Task4.V16.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = x * 2 < y * 2 ? Math.Pow(1 + (1 / Math.Pow(y, 2)), x) : Math.Pow(y - (1 / Math.Pow(y, 2));
+            double z = x * 2 < y * 2 ? Math.Pow(1 + (1 / Math.Pow(y, 2)), x) : y - (1 / Math.Pow(x, 2));
             return Math.Round(z, 3);
         }
     }
