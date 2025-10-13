88cf2cb6bb16db48dc900ecbe115f3ea1c6f5675
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-13 22:36:00 +0500

    Task6

---
+++ Tyuiu.MolokanovNK.Sprint2.Task6.V5.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MolokanovNK.Sprint2.Task6.V5.Lib
+{
+    public class DataService : ISprint2Task6V5
+    {
+        public string FindCardValue(int value)
+        {
+            switch (value)
+            {
+                case 6: return "Шестерка";
+                case 7: return "Семерка";
+                case 8: return "Восьмерка";
+                case 9: return "Девятка";
+                case 10: return "Десятка";
+                case 11: return "Валет";
+                case 12: return "Дама";
+                case 13: return "Король";
+                case 14: return "Туз";
+                default: throw new ArgumentException($"Достоинство карты от 6 до 14. Значение {value}");
+            }
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint2.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.MolokanovNK.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint2.Task6.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual("шестерка", ds.FindCardValue(6));
+            Assert.AreEqual("семерка", ds.FindCardValue(7));
+            Assert.AreEqual("восьмерка", ds.FindCardValue(8));
+            Assert.AreEqual("девятка", ds.FindCardValue(9));
+            Assert.AreEqual("десятка", ds.FindCardValue(10));
+            Assert.AreEqual("валет", ds.FindCardValue(11));
+            Assert.AreEqual("дама", ds.FindCardValue(12));
+            Assert.AreEqual("король", ds.FindCardValue(13));
+            Assert.AreEqual("туз", ds.FindCardValue(14));
+
+            Assert.ThrowsException<ArgumentException>(() => ds.FindCardValue(15));
+            Assert.ThrowsException<ArgumentException>(() => ds.FindCardValue(5));
+        }
+    }    
+}
