a1787f9631d7872535947f8dd4278d174352f4df
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-13 21:45:21 +0500

    Task5

--- Tyuiu.MolokanovNK.Sprint2.Task5.V15.Lib/DataService.cs
+++ Tyuiu.MolokanovNK.Sprint2.Task5.V15.Lib/DataService.cs
@@ -6,35 +6,21 @@ namespace Tyuiu.MolokanovNK.Sprint2.Task5.V15.Lib
     {
         public string FindDayName(int k)
         {
-            
+
             string res;
             switch (k)
             {
-                case 1:
-                    res = "Понедельник";
-                    break;
-                case 2:
-                    res = "Вторник";
-                    break;
-                case 3:
-                    res = "Среда";
-                    break;
-                case 4:
-                    res = "Четверг";
-                    break;
-                case 5:
-                    res = "Пятница";
-                    break;
-                case 6:
-                    res = "Суббота";
-                    break;
-                case 7:
-                    res = "Воскресенье";
-                    break;
-                default:
-                    throw new ArgumentException($"1 января должен быть понедельник. Значение {k}");
+                case 1: res = "Понедельник"; break;
+                case 2: res = "Вторник"; break;
+                case 3: res = "Среда"; break;
+                case 4: res = "Четверг"; break;
+                case 5: res = "Пятница"; break;
+                case 6: res = "Суббота"; break;
+                case 7: res = "Воскресенье"; break;
+                default: throw new ArgumentException($"1 января должен быть понедельник. Значение {k}");
             }
             return res;
+
         }
     }
 }
