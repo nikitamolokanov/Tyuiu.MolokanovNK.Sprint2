8d86a08dafa85742b5c10118863f528cf4fb3675
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-10-13 22:37:14 +0500

    Task6

--- Tyuiu.MolokanovNK.Sprint2.Task6.V5.Lib/DataService.cs
+++ Tyuiu.MolokanovNK.Sprint2.Task6.V5.Lib/DataService.cs
@@ -8,15 +8,15 @@ namespace Tyuiu.MolokanovNK.Sprint2.Task6.V5.Lib
         {
             switch (value)
             {
-                case 6: return "Шестерка";
-                case 7: return "Семерка";
-                case 8: return "Восьмерка";
-                case 9: return "Девятка";
-                case 10: return "Десятка";
-                case 11: return "Валет";
-                case 12: return "Дама";
-                case 13: return "Король";
-                case 14: return "Туз";
+                case 6: return "шестерка";
+                case 7: return "семерка";
+                case 8: return "восьмерка";
+                case 9: return "девятка";
+                case 10: return "десятка";
+                case 11: return "валет";
+                case 12: return "дама";
+                case 13: return "король";
+                case 14: return "туз";
                 default: throw new ArgumentException($"Достоинство карты от 6 до 14. Значение {value}");
             }
         }
